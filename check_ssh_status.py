819d19330afee0c013b84c8deb7674bfe9f2ba22
Daekun Kim <dkkim6200@gmail.com>
2020-09-19 19:00:30 -0400

    Added case handling for terminated and non-running instances

--- check_ssh_status.py
+++ check_ssh_status.py
@@ -7,13 +7,16 @@ import socket
 #messages="Following Instances have port 22 open"
 
 class InstanceReachability:
-    def __init__(self, instance_id, private_ip_address, reachable):
+    def __init__(self, instance_id, private_ip_address = "", reachable = False):
         self.instance_id = instance_id
         self.private_ip_address = private_ip_address
         self.reachable = reachable
     
     def __str__(self):
-        return "Instance " + self.instance_id + " with private IP address " + self.private_ip_address + " at port 22 is " + ("reachable." if self.reachable else "not reachable.")
+        if (self.private_ip_address != ""):
+            return "Instance " + self.instance_id + " with private IP address " + self.private_ip_address + " at port 22 is " + ("reachable." if self.reachable else "not reachable.")
+        else:
+            return "Instance " + self.instance_id + " is not reachable."
         
 
 def lambda_handler(event, context):
@@ -24,15 +27,20 @@ def lambda_handler(event, context):
     
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
+            if (instance["State"]["Code"] != 16):
+                instance_reachability = InstanceReachability(instance["InstanceId"], reachable = True)
+                instance_reachabilities.append(instance_reachability)
+                continue
+            
             reachable = False
             
             security_group_ids = list((group["GroupId"] for group in instance["SecurityGroups"]))
             for sg in connection.describe_security_groups(GroupIds = security_group_ids)["SecurityGroups"]:
                 for rule in sg["IpPermissions"]:
-                    if (rule["FromPort"] == 22 and rule["ToPort"] == 22) and all(ip_range["CidrIp"] == "0.0.0.0/0" for ip_range in rule["IpRanges"]):
-                        reachable = instance["State"]["Name"] == "running"
-            
-            instance_reachability = InstanceReachability(instance["InstanceId"], instance["PrivateIpAddress"], reachable)
+                    if rule["IpProtocol"] == "tcp" and (rule["FromPort"] == 22 and rule["ToPort"] == 22) and all(ip_range["CidrIp"] == "0.0.0.0/0" for ip_range in rule["IpRanges"]):
+                        reachable = True
+                        
+            instance_reachability = InstanceReachability(instance["InstanceId"], instance["PrivateIpAddress"], True)
             instance_reachabilities.append(instance_reachability)
     
     for instance_reachability in instance_reachabilities:
