3521e5e2af5970d781f2a9a40e933847da2b09e9
Daekun Kim <dkkim6200@gmail.com>
2020-09-20 23:16:09 -0400

    Added documentation

---
+++ README.md
@@ -0,0 +1,54 @@
+# Promethium AWS Challenge
+
+## Introduction
+
+This project tackles Promethium's AWS Challenge. The project creates the following:
+* Private EC2 instance that only allows outbound access to the Internet.
+    * No public IP.
+    * Outsider can only respond to requests, and no access shall be allows to the EC2 instance in any other circumstance.
+    * Inbound access allowed within the same VPC.
+* Lambda function that periodically checks the status of port 22 for the EC2 instance created above.
+
+
+## Tech Stack
+
+* AWS CloudFormation
+    * Used to create the AWS VPC, EC2, Lambda stack.
+* Python 3.8
+    * Used for the Lambda function.
+
+
+## Steps for VPC + EC2 + Lambda Creation
+
+1. Create VPC.
+    * CIDR: 10.100.0.0/16
+1. Create Internet gateway for the public subnet.
+1. Attach Internet gateway to VPC.
+1. Create 2 subnets: 1 public, 1 private.
+    * Public: 10.100.1.0/24
+    * Private: 10.100.2.0/24
+1. Create a NAT gateway for the private subnet.
+1. Create a Route Table for private subnet.
+    * Add 0.0.0.0/0 nat-GW_ID to the routes.
+1. Create a Route Table for public subnet.
+    * Add 0.0.0.0/0 igw-GW_ID to the routes.
+1. Change route table association for 2 subnets.
+1. Create a security group.
+    * Allow SSH from anywhere within the VPC (10.100.0.0/16 at port 22)
+1. Create EC2 instance.
+    1. Change Network to the VPC I created from #1.
+    1. Change Subnet to private subnet from #4.
+    1. Change security group to group from #9.
+1. Create an IAM role with the following policies:
+    * AmazonEC2ReadOnlyAccess
+    * AWSLambdaBasicExecutionRole
+1. Create Lambda function for checking port 22 status.
+    * **NOTE**: The Python 3.8 code must be uploaded to S3.
+1. Create a scheduled event that calls the Lambda function
+    * Rate: 1 minute
+    * Must be provided with a permission `lambda:InvokeFunction`
+
+
+## Output Stack
+
+IMAGE
--- check_ssh_status.py
+++ check_ssh_status.py
@@ -1,13 +1,28 @@
+"""This Lambda module checks the status of the port 22 of EC2 instance created by AWS CloudFormation template.
+
+**Note**: INSTANCE_ID and VPC_CIDR is required as part of the environment variables.
+"""
+
+__docformat__ = 'reStructuredText'
+
 import sys
 import boto3
 from boto3 import ec2
 import socket
-
-#listOfInstances=""
-#messages="Following Instances have port 22 open"
+import os
 
 class InstanceReachability:
+    """This class is a container for information about reachability of an EC2 instance at port 22.
+
+    Use this class to print a prettified version of the reachability information.
+    """
     def __init__(self, instance_id, private_ip_address = "", reachable = False):
+        """Initializes a new InstanceReachability object.
+
+        :param instance_id: Instance ID of an AWS EC2 instance.
+        :param private_ip_address: Private ID of the AWS EC2 instance. If the default value \"\" is used, the private IP address will not be used in __str__ method.
+        :param reachable: Reachability of the EC2 instance at port 22
+        """
         self.instance_id = instance_id
         self.private_ip_address = private_ip_address
         self.reachable = reachable
@@ -20,32 +35,49 @@ class InstanceReachability:
         
 
 def lambda_handler(event, context):
+    """Prints status of port 22 that was created from CloudFormation template.
+
+    .. note::
+        INSTANCE_ID and VPC_CIDR is required as part of the environment variables.
+
+    :returns: OK status response if the code executes successfully.
+    """
+
     instance_reachabilities = []
     
     connection = boto3.client('ec2', region_name='us-east-2')
-    response = connection.describe_instances()
+    response = connection.describe_instances(InstanceIds=[os.environ['INSTANCE_ID']])
     
+    # Made into a loop for scalability to multiple instances
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
+            # If the instance is not running, then add to instance_reachabilities an unreachable instance
             if (instance["State"]["Code"] != 16):
-                instance_reachability = InstanceReachability(instance["InstanceId"], reachable = True)
+                instance_reachability = InstanceReachability(instance["InstanceId"], reachable = False)
                 instance_reachabilities.append(instance_reachability)
                 continue
             
             reachable = False
             
+            # Iterate through the security groups attached to the instance
             security_group_ids = list((group["GroupId"] for group in instance["SecurityGroups"]))
             for sg in connection.describe_security_groups(GroupIds = security_group_ids)["SecurityGroups"]:
+                # Iterate through the NACL of the security group
                 for rule in sg["IpPermissions"]:
-                    if rule["IpProtocol"] == "tcp" and (rule["FromPort"] == 22 and rule["ToPort"] == 22) and all(ip_range["CidrIp"] == "0.0.0.0/0" for ip_range in rule["IpRanges"]):
+                    # If any of the NACL rules allow inbound traffic to port 22 from within the VPC, then the instance is reachable on port 22
+                    if rule["IpProtocol"] == "tcp" and (rule["FromPort"] == 22 and rule["ToPort"] == 22) and all(ip_range["CidrIp"] == os.environ['VPC_CIDR'] for ip_range in rule["IpRanges"]):
                         reachable = True
-                        
-            instance_reachability = InstanceReachability(instance["InstanceId"], instance["PrivateIpAddress"], True)
+                        continue
+            
+            # Add to instance_reachabilities an instance along with the reachability information
+            instance_reachability = InstanceReachability(instance["InstanceId"], instance["PrivateIpAddress"], reachable)
             instance_reachabilities.append(instance_reachability)
     
+    # Print the instance reachability infos
     for instance_reachability in instance_reachabilities:
         print(instance_reachability)
-        
+    
+    # The function executed successfully, so responde with 'Success'.
     return {
         'statusCode': 200,
         'body': "Lambda function executed successfully."
